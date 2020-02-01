13a5f8fbc19821c7483834db059cdaa32d04265a
Harpes <823289099@qq.com>
2020-02-01 17:48:53 +0800

    抽卡功能

--- Chloe/group_chats/scheduler.py
+++ Chloe/group_chats/scheduler.py
@@ -15,25 +15,25 @@ async def scheduler_reminder(msg: str):
 
 
 # 1450
-@nonebot.scheduler.scheduler_job('cron', hour=14, minute=50, misfire_grace_time=30)
+@nonebot.scheduler.scheduled_job('cron', hour=14, minute=50, misfire_grace_time=30)
 async def _():
     msg = '背刺Time背刺Time背刺Time背刺Time背刺Time!!!\n大家记得背刺群主'
     await scheduler_reminder(msg)
 
 # 1456
-@nonebot.scheduler.scheduler_job('cron', hour=14, minute=56, misfire_grace_time=30)
+@nonebot.scheduler.scheduled_job('cron', hour=14, minute=56, misfire_grace_time=30)
 async def _():
     msg = str(datetime.now())
     await scheduler_reminder(msg)
 
 # 0800
-@nonebot.scheduler.scheduler_job('cron', hour=8, misfire_grace_time=30)
+@nonebot.scheduler.scheduled_job('cron', hour=8, misfire_grace_time=30)
 async def _():
     msg = '起床啦！！！'
     await scheduler_reminder(msg)
 
 # 2300
-@nonebot.scheduler.scheduler_job('cron', hour=23, misfire_grace_time=30)
+@nonebot.scheduler.scheduled_job('cron', hour=23, misfire_grace_time=30)
 async def _():
     msg = '现在23点，骑士君该睡觉了！'
     await scheduler_reminder(msg)
