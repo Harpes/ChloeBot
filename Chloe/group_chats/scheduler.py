65a6784c24649ada396caacad5314f5fe0c67087
Harpes <823289099@qq.com>
2020-02-02 22:01:41 +0800

    bug fix

--- Chloe/group_chats/scheduler.py
+++ Chloe/group_chats/scheduler.py
@@ -9,31 +9,31 @@ async def scheduler_reminder(msg: str):
     group_list = await bot.get_group_list()
     try:
         for group in group_list:
-            await bot.send_group_msg(group_id=group, message=msg)
+            await bot.send_group_msg(group_id=group['group_id'], message=msg)
     except:
         pass
 
 
 # 1450
-@nonebot.scheduler.scheduled_job('cron', hour=14, minute=50, misfire_grace_time=30)
+@nonebot.scheduler.scheduled_job('cron', hour='14', minute='50', misfire_grace_time=30)
 async def _():
     msg = '背刺Time背刺Time背刺Time背刺Time背刺Time!!!\n大家记得背刺群主'
     await scheduler_reminder(msg)
 
 # 1456
-@nonebot.scheduler.scheduled_job('cron', hour=14, minute=56, misfire_grace_time=30)
+@nonebot.scheduler.scheduled_job('cron', hour='14', minute='56', misfire_grace_time=30)
 async def _():
     msg = str(datetime.now())
     await scheduler_reminder(msg)
 
 # 0800
-@nonebot.scheduler.scheduled_job('cron', hour=8, misfire_grace_time=30)
+@nonebot.scheduler.scheduled_job('cron', hour='8', misfire_grace_time=30)
 async def _():
     msg = '起床啦！！！'
     await scheduler_reminder(msg)
 
-# 2300
-@nonebot.scheduler.scheduled_job('cron', hour=23, misfire_grace_time=30)
+# 2330
+@nonebot.scheduler.scheduled_job('cron', hour='23', minute='30', misfire_grace_time=30)
 async def _():
-    msg = '现在23点，骑士君该睡觉了！'
+    msg = '现在23:30，骑士君该睡觉了！'
     await scheduler_reminder(msg)
