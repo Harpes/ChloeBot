5332ec8652ecad1d86c29b13a3ef73e640a9903a
Harpes <823289099@qq.com>
2020-04-02 23:19:32 +0800

    完善报刀

--- Chloe/clanbattle/battleMaster.py
+++ Chloe/clanbattle/battleMaster.py
@@ -16,8 +16,7 @@ class BattleMaster(object):
     def __init__(self):
         self.databaseObj = DataBaseIO()
 
-    @staticmethod
-    def get_stage(r: int) -> int:
+    def get_stage(self, r: int) -> int:
         if r >= 35:
             return 4
         elif r >= 11:
@@ -27,23 +26,21 @@ class BattleMaster(object):
         else:
             return 1
 
-    @staticmethod
-    def get_boss_hp(r: int, boss: int, server: int) -> int:
-        stage = BattleMaster.get_stage(r)
+    def get_boss_hp(self, r: int, boss: int, server: int) -> int:
+        stage = self.get_stage(r)
         config = get_config()
         return config[config["BOSS_HP"][server]][stage - 1][boss - 1]
 
-    @staticmethod
-    def get_next_boss(r: int, boss: int) -> (int, int):
+    def get_next_boss(self, r: int, boss: int) -> (int, int):
         boss += 1
         if boss > 5:
             boss = 1
             r += 1
+
         return (r, boss)
 
-    @staticmethod
-    def get_score_rate(r: int, boss: int, server: int) -> float:
-        stage = BattleMaster.get_stage(r)
+    def get_score_rate(self, r: int, boss: int, server: int) -> float:
+        stage = self.get_stage(r)
         config = get_config()
         return config[config["SCORE_RATE"][server]][stage - 1][boss - 1]
 
@@ -81,17 +78,17 @@ class BattleMaster(object):
             boss = recs[-1]['boss']
             for rec in recs[::-1]:
                 flag, ri, bi = rec['flag'], rec['round'], rec['boss']
-                if flag == 1:
-                    r, boss = BattleMaster.get_next_boss(ri, bi)
-                    dmg = 0
-                    break
-
                 if ri == r and bi == boss:
-                    dmg += rec['dmg']
+                    if flag == 1 or flag == 3:
+                        r, boss = self.get_next_boss(ri, bi)
+                        dmg = 0
+                        break
+                    else:
+                        dmg += rec['dmg']
                 else:
                     break
 
-        hp = BattleMaster.get_boss_hp(r, boss, server) - dmg
+        hp = self.get_boss_hp(r, boss, server) - dmg
 
         return (name, r, boss, hp)
 
