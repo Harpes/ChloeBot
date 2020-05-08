7265de8de8d45742067bda69e1773240fa110719
Harpes <823289099@qq.com>
2020-05-08 22:08:33 +0800

    修改一下抽卡的配置机制
    感谢IceCoffee的资源

--- Chloe/group_chats/gacya.py
+++ Chloe/group_chats/gacya.py
@@ -1,3 +1,4 @@
+import json
 import math
 import os
 import random as rd
@@ -6,47 +7,27 @@ import nonebot
 from nonebot import CommandSession, on_command
 from PIL import Image
 
-imgRoot = os.path.join(os.path.dirname(__file__), 'image')
-if not os.path.exists(os.path.join(imgRoot, 'out')):
-    os.mkdir(os.path.join(imgRoot, 'out'))
+from .chara import gen_chara_avatar
 
+imgOut = os.path.join(os.path.dirname(__file__), 'out')
+if not os.path.exists(imgOut):
+    os.mkdir(imgOut)
 
-gacya3 = ['杏奈', '真步', '璃乃',
-          '初音', '霞', '伊緒',
-          '咲戀', '望', '妮諾', '秋乃',
-          '鏡華', '智', '真琴',
-          '伊莉亞', '純', '靜流',
-          '莫妮卡', '流夏', '吉塔',
-          '亞里莎', '安', '古蕾婭',
-          '空花（大江戶）', '妮諾（大江戶）', '碧（插班生）',
-          '克蘿依', '露娜', '嘉夜', 'イリヤ（クリスマス）']
+config = json.load(
+    open(os.path.join(os.path.dirname(__file__), 'gacya.json'), 'r', encoding='utf8'))
+gacyaUp = config['gacyaUp']
+gacyaFes = config['gacyaFes']
+gacya3 = config['gacya3']
+gacya2 = config['gacya2']
+gacya1 = config['gacya1']
 
-gacya2 = ['茉莉', '茜里', '宮子',
-          '雪', '七七香', '美里',
-          '鈴奈', '香織', '美美',
-          '綾音', '鈴', '惠理子',
-          '忍', '真陽', '栞',
-          '千歌', '空花', '珠希',
-          '美冬', '深月', '紡希']
-
-gacya1 = ['日和', '怜', '禊', '胡桃', '依里', '鈴莓', '優花梨', '碧', '美咲', '莉瑪', '步未']
-
-gacyaFes = ['矛依未', '克莉絲提娜', '似似花']
-
-gacyaUp = ['キャル（ニューイヤー）']
-
-stones = [50, 10, 1]
-
-# up, FES, 3星, 2星
-percents = [0.7, 0, 1.8, 20.5]  # 普通
-# percents = [1.4, 0, 3.6, 18]  # up双倍
-# percents = [0.7, 0.6, 4.3, 18]  # FES三星双倍
-
+percents = [config['pUp'], config['pFes'], config['p3'], config['p2']]
 pUp = percents[0]
 pFes = pUp + percents[1]
 p3 = pFes + percents[2]
 p2 = p3 + percents[3]
 
+stones = [50, 10, 1]
 img_size = 72
 
 
@@ -59,19 +40,18 @@ async def _(session: CommandSession):
     pic = ''
     i = rd.random() * 100
     if i <= pUp:
-        pic = rd.choice(gacyaUp)
+        pic = gen_chara_avatar(rd.choice(gacyaUp), 3)
     elif i <= pFes:
-        pic = rd.choice(gacyaFes)
+        pic = gen_chara_avatar(rd.choice(gacyaFes), 3)
     elif i <= p3:
-        pic = rd.choice(gacya3)
+        pic = gen_chara_avatar(rd.choice(gacya3), 3)
     elif i <= p2:
-        pic = rd.choice(gacya2)
+        pic = gen_chara_avatar(rd.choice(gacya2), 2)
     else:
-        pic = rd.choice(gacya1)
+        pic = gen_chara_avatar(rd.choice(gacya1), 1)
 
-    pic = f'[CQ:image,file=file:///{imgRoot}\\{pic}.png]'
-
-    await session.send(msg + pic)
+    pic.save(os.path.join(imgOut, 's.png'))
+    await session.send(msg + f'[CQ:image,file=file:///{os.path.join(imgOut, "s.png")}]')
 
 
 @on_command('单抽到up', aliases=('單抽到up', ), only_to_me=False)
@@ -111,12 +91,12 @@ async def _(session: CommandSession):
     background = Image.new('RGB', (width, height), 'white')
     name = session.ctx['user_id']
     for index, cha in enumerate(result):
-        pic = Image.open(f'{imgRoot}\\{cha}.png').resize((img_size, img_size))
+        pic = gen_chara_avatar(cha, 3).resize((img_size, img_size))
         col = index % row_nums
         row = index // row_nums
         background.paste(pic, (col * img_size, row * img_size))
-    background.save(imgRoot + f'\\out\\{name}.png', quality=100)
-    msg += f'\n[CQ:image,file=file:///{imgRoot}\\out\\{name}.png]'
+    background.save(imgOut + f'\\{name}.png', quality=100)
+    msg += f'\n[CQ:image,file=file:///{imgOut}\\{name}.png]'
 
     msg += f'\n共计{n3}个三星，{n2}个两星，{n1}个一星'
 
@@ -137,23 +117,23 @@ async def _(session: CommandSession):
     for x in range(10):
         i = rd.random() * 100
         if i <= pUp:
-            result.append(rd.choice(gacyaUp))
+            result.append([rd.choice(gacyaUp), 3])
             n3 += 1
         elif i <= pFes:
-            result.append(rd.choice(gacyaFes))
+            result.append([rd.choice(gacyaFes), 3])
             n3 += 1
         elif i <= p3:
-            result.append(rd.choice(gacya3))
+            result.append([rd.choice(gacya3), 3])
             n3 += 1
         elif i <= p2:
-            result.append(rd.choice(gacya2))
+            result.append([rd.choice(gacya2), 2])
             n2 += 1
         else:
             if x == 9:
-                result.append(rd.choice(gacya2))
+                result.append([rd.choice(gacya2), 2])
                 n2 += 1
             else:
-                result.append(rd.choice(gacya1))
+                result.append([rd.choice(gacya1), 1])
                 n1 += 1
 
     msg += f'获得{n3 * stones[0] + n2 * stones[1] + n1 * stones[2]}个无名之石'
@@ -163,12 +143,12 @@ async def _(session: CommandSession):
     a = 0
     for x in range(5):
         for y in range(2):
-            pic = Image.open(f'{imgRoot}\\{result[a]}.png').resize(
-                (img_size, img_size))
+            cha, star = result[a]
+            pic = gen_chara_avatar(cha, star).resize((img_size, img_size))
             background.paste(pic, (x * img_size, y * img_size))
             a += 1
-    background.save(imgRoot + f'\\out\\{name}.png', quality=100)
-    msg += f'[CQ:image,file=file:///{imgRoot}\\out\\{name}.png]'
+    background.save(imgOut + f'\\{name}.png', quality=100)
+    msg += f'[CQ:image,file=file:///{imgOut}\\{name}.png]'
 
     await session.send(msg)
 
@@ -214,12 +194,12 @@ async def _(session: CommandSession):
     background = Image.new('RGB', (width, height), 'white')
     name = session.ctx['user_id']
     for index, cha in enumerate(result):
-        pic = Image.open(f'{imgRoot}\\{cha}.png').resize((img_size, img_size))
+        pic = gen_chara_avatar(cha, 3).resize((img_size, img_size))
         col = index % row_nums
         row = index // row_nums
         background.paste(pic, (col * img_size, row * img_size))
-    background.save(imgRoot + f'\\out\\{name}.png', quality=100)
-    msg += f'\n[CQ:image,file=file:///{imgRoot}\\out\\{name}.png]'
+    background.save(imgOut + f'\\{name}.png', quality=100)
+    msg += f'\n[CQ:image,file=file:///{imgOut}\\{name}.png]'
 
     msg += f'\n共计{n3}个三星，{n2}个两星，{n1}个一星'
 
@@ -265,12 +245,12 @@ async def _(session: CommandSession):
     background = Image.new('RGB', (width, height), 'white')
     name = session.ctx['user_id']
     for index, cha in enumerate(result):
-        pic = Image.open(f'{imgRoot}\\{cha}.png').resize((img_size, img_size))
+        pic = gen_chara_avatar(cha, 3).resize((img_size, img_size))
         col = index % row_nums
         row = index // row_nums
         background.paste(pic, (col * img_size, row * img_size))
-    background.save(imgRoot + f'\\out\\{name}.png', quality=100)
-    msg += f'\n[CQ:image,file=file:///{imgRoot}\\out\\{name}.png]'
+    background.save(imgOut + f'\\{name}.png', quality=100)
+    msg += f'\n[CQ:image,file=file:///{imgOut}\\{name}.png]'
 
     msg += f'\n共计{n3}个三星，{n2}个两星，{n1}个一星'
 
