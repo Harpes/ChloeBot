638a1397ed96454ccd1fd4b2635f3375c14f0807
Harpes <823289099@qq.com>
2020-07-04 21:43:34 +0800

    数据库组织方式更新，预约移入数据库

--- Chloe/clanbattle/__init__.py
+++ Chloe/clanbattle/__init__.py
@@ -1,5 +1,48 @@
 import base64
+import json
+import os
 from datetime import datetime, timedelta
+
+DB_folder = 'clanbattle'
+CLAN_CONFIGS = os.path.join(DB_folder, 'groups.json')
+if not os.path.exists(DB_folder):
+    os.mkdir(DB_folder)
+
+
+def get_configs() -> dict:
+    if os.path.exists(CLAN_CONFIGS):
+        return json.load(open(CLAN_CONFIGS, 'r'))
+    return {}
+
+
+def save_configs(configs: dict):
+    json.dump(configs, open(CLAN_CONFIGS, 'w'))
+
+
+def add_clan(gid: int, name: str, server: int):
+    configs = get_configs()
+    configs[str(gid)] = [name, server]
+    save_configs(configs)
+
+
+def get_clan(gid: int) -> [str, int]:
+    return get_configs().get(str(gid), [None, -1])
+
+
+def del_clan(gid: int):
+    configs = get_configs()
+    if str(gid) in configs:
+        del configs[str(gid)]
+    save_configs(configs)
+
+
+def get_month(target_time: datetime = None) -> str:
+    target = target_time or datetime.now()
+    day = target - timedelta(hours=5)
+    if day.day < 20:
+        day -= timedelta(days=day.day)
+
+    return day.strftime("%Y%m")
 
 
 def get_start_of_day(target_time: datetime = None) -> datetime:
--- Chloe/clanbattle/database/sqlite.py
+++ Chloe/clanbattle/database/sqlite.py
@@ -1,136 +1,220 @@
 import os
 import sqlite3
-from datetime import datetime, timedelta, timezone
+from datetime import datetime
 
-DB_folder = 'clanbattle'
-if not os.path.exists(DB_folder):
-    os.mkdir(DB_folder)
-
-
-def getMonth(time: datetime) -> str:
-    day = time - timedelta(hours=5)
-    if day.day < 20:
-        day -= timedelta(days=day.day)
-
-    return day.strftime("%Y%m")
+from .. import DB_folder, get_month
 
 
 class DataBaseIO(object):
-    def connect(self, gid: int):
+    def __connect(self, gid: int) -> sqlite3.Connection:
         db_path = os.path.join(DB_folder, '%s.db' % (gid, ))
         return sqlite3.connect(db_path)
 
-    def createTable(self, gid: int, table_name: str, cols: list) -> sqlite3.Connection:
-        conn = self.connect(gid)
+    def __create_table(self, gid: int, table_name: str, cols: list) -> (sqlite3.Connection, str):
+        conn = self.__connect(gid)
+        table_name = '%s_%s' % (table_name, get_month())
         create_sql = 'CREATE TABLE IF NOT EXISTS %s(%s)' % (
             table_name, ','.join(cols))
         conn.execute(create_sql)
-        return conn
+        return (conn, table_name)
 
-    def _createClan(self, gid: int) -> sqlite3.Connection:
-        return self.createTable(gid, 'clan', [
-            'gid INT PRIMARY KEY NOT NULL', 'name TEXT NOT NULL', 'server INT NOT NULL'])
+    def _create_member(self, gid: int) -> (sqlite3.Connection, str):
+        columns = [
+            'uid INT PRIMARY KEY',
+            'name TEXT NOT NULL',
+        ]
+        return self.__create_table(gid, 'member', columns)
 
-    def addClan(self, gid: int, name: str, server: int):
-        conn = self._createClan(gid)
+    def add_member(self, gid: int, uid: int, name: str):
+        conn, table_name = self._create_member(gid)
 
-        insert_sql = 'INSERT INTO clan VALUES (%s, "%s", %s)' % (
-            gid, name, server)
+        insert_sql = 'INSERT INTO %s VALUES (%s, "%s")' % (
+            table_name, uid, name)
         conn.execute(insert_sql)
-
         conn.commit()
         conn.close()
 
-    def getClan(self, gid: int) -> list:
-        conn = self._createClan(gid)
+    def get_member(self, gid: int, uid: int = None) -> list:
+        conn, table_name = self._create_member(gid)
         cur = conn.cursor()
 
-        select_sql = 'SELECT name, server FROM clan WHERE gid = %s' % (gid, )
-        cur.execute(select_sql)
-        result = cur.fetchone()
-
-        conn.commit()
-        conn.close()
-
-        return result
-
-    def _createMember(self, gid: int) -> (sqlite3.Connection, str):
-        month = getMonth(datetime.now())
-        return (self.createTable(gid, 'member' + month, ['gid INT NOT NULL', 'uid INT NOT NULL',
-                                                         'name TEXT NOT NULL', 'PRIMARY KEY (gid, uid)']), month)
-
-    def addMember(self, gid: int, uid: int, name: str):
-        conn, month = self._createMember(gid)
-
-        insert_sql = 'INSERT INTO member%s VALUES (%s, %s, "%s")' % (
-            month, gid, uid, name)
-        conn.execute(insert_sql)
-
-        conn.commit()
-        conn.close()
-
-    def getMember(self, gid: int, uid: int = None):
-        conn, month = self._createMember(gid)
-        cur = conn.cursor()
-
-        select_sql = 'SELECT uid, name FROM member%s WHERE gid = %s' % (
-            month, gid)
-        if not uid is None:
-            select_sql += ' AND uid = %s' % (uid, )
+        select_sql = 'SELECT uid, name FROM %s' % (table_name, )
+        if uid is not None:
+            select_sql += ' WHERE uid = %s' % (uid, )
         cur.execute(select_sql)
         result = cur.fetchall()
-
-        conn.commit()
         conn.close()
 
         return result
 
-    def _createRecs(self, gid: int) -> (sqlite3.Connection, str):
-        month = getMonth(datetime.now())
-        return (self.createTable(gid, 'rec' + month, ['recid INTEGER PRIMARY KEY AUTOINCREMENT',
-                                                      'gid INT NOT NULL', 'uid INT NOT NULL',
-                                                      'time TIMESTAMP NOT NULL', 'round INT NOT NULL',
-                                                      'boss INT NOT NULL', 'dmg INT NOT NULL',
-                                                      'flag INT NOT NULL']), month)
+    def del_member(self, gid: int, uid: int):
+        conn, table_name = self._create_member(gid)
 
-    # flag: 0:正常刀 1:尾刀 2:余刀 3:余尾刀
-    def addRec(self, gid: int, uid: int, r: int, b: int, dmg: int, flag: int):
-        conn, month = self._createRecs(gid)
-
-        insert_sql = 'INSERT INTO rec%s VALUES (NULL, %s, %s, "%s", %s, %s, %s, %s)' % (
-            month, gid, uid, datetime.now().strftime('%Y/%m/%d %H:%M'), r, b, dmg, flag)
-        conn.execute(insert_sql)
-
+        delete_sql = 'DELETE FROM %s WHERE uid = %s' % (table_name, uid)
+        conn.execute(delete_sql)
         conn.commit()
         conn.close()
 
-    def getRec(self, gid: int, uid: int = None, start: datetime = None, end: datetime = None) -> list:
-        conn, month = self._createRecs(gid)
+    def _create_rec(self, gid: int) -> (sqlite3.Connection, str):
+        columns = [
+            'recid INTEGER PRIMARY KEY AUTOINCREMENT',
+            'uid INT NOT NULL',
+            'time TIMESTAMP NOT NULL', 'round INT NOT NULL',
+            'boss INT NOT NULL', 'dmg INT NOT NULL',
+            'flag INT NOT NULL', 'remark TEXT NOT NULL'
+        ]
+        return self.__create_table(gid, 'recs', columns)
+
+    # flag: 0:正常刀 1:尾刀 2:余刀 3:余尾刀
+    def add_rec(self, gid: int, uid: int, r: int, b: int, dmg: int, flag: int, remark: str):
+        conn, table_name = self._create_rec(gid)
+
+        insert_sql = 'INSERT INTO %s VALUES (NULL, %s, "%s", %s, %s, %s, %s, "%s")' % (
+            table_name, uid, datetime.now().strftime('%Y/%m/%d %H:%M'), r, b, dmg, flag, remark)
+        conn.execute(insert_sql)
+        conn.commit()
+        conn.close()
+
+    def get_rec(self, gid: int, uid: int = None, start: datetime = None, end: datetime = None) -> list:
+        conn, table_name = self._create_rec(gid)
         cur = conn.cursor()
 
-        select_sql = 'SELECT * FROM rec%s WHERE gid = %s' % (month, gid)
-        if not uid is None:
+        select_sql = 'SELECT * FROM %s WHERE 0 = 0' % (table_name, )
+        if uid is not None:
             select_sql += ' AND uid = %s' % (uid, )
-        if not start is None:
+        if start is not None:
             select_sql += ' AND time > "%s"' % (
                 start.strftime('%Y/%m/%d %H:%M'), )
-        if not end is None:
+        if end is not None:
             select_sql += ' AND time < "%s"' % (
                 end.strftime('%Y/%m/%d %H:%M'), )
 
         cur.execute(select_sql)
         result = cur.fetchall()
-
-        conn.commit()
         conn.close()
 
         return result
 
-    def delRec(self, gid: int, recid: int):
-        conn, month = self._createRecs(gid)
+    def del_rec(self, gid: int, recid: int):
+        conn, table_name = self._create_rec(gid)
 
-        delete_sql = 'DELETE FROM rec%s WHERE recid = %s' % (month, recid)
+        delete_sql = 'DELETE FROM %s WHERE recid = %s' % (table_name, recid)
         conn.execute(delete_sql)
+        conn.commit()
+        conn.close()
 
+    def clear_rec(self, gid: int):
+        conn, table_name = self._create_rec(gid)
+
+        drop_sql = 'DROP TABLE ' + table_name
+        conn.execute(drop_sql)
+        conn.commit()
+        conn.close()
+
+    def _create_enter(self, gid: int) -> (sqlite3.Connection, str):
+        columns = [
+            'recid INTEGER PRIMARY KEY AUTOINCREMENT',
+            'uid INT NOT NULL',
+            'time TIMESTAMP NOT NULL',
+            'remark TEXT NOT NULL',
+            'flag INT NOT NULL',
+            'valid INT NOT NULL'
+        ]
+        return self.__create_table(gid, 'enter', columns)
+
+    # flag: 0:正常 1:挂树
+    def add_enter(self, gid: int, uid: int, remark: str, flag: int):
+        conn, table_name = self._create_enter(gid)
+
+        insert_sql = 'INSERT INTO %s VALUES (NULL, %s, "%s", "%s", %s, 1)' % (
+            table_name, uid, datetime.now().strftime('%Y/%m/%d %H:%M'), remark, flag)
+        conn.execute(insert_sql)
+        conn.commit()
+        conn.close()
+
+    def get_enter(self, gid: int, uid: int = None, start: datetime = None, end: datetime = None, flag: int = None) -> list:
+        conn, table_name = self._create_enter(gid)
+        cur = conn.cursor()
+
+        select_sql = 'SELECT * FROM %s WHERE valid = 1' % (table_name, )
+        if uid is not None:
+            select_sql += ' AND uid = %s' % (uid, )
+        if start is not None:
+            select_sql += ' AND time > "%s"' % (
+                start.strftime('%Y/%m/%d %H:%M'), )
+        if end is not None:
+            select_sql += ' AND time < "%s"' % (
+                end.strftime('%Y/%m/%d %H:%M'), )
+        if flag is not None:
+            select_sql += ' AND flag = %s' % (flag, )
+
+        cur.execute(select_sql)
+        result = cur.fetchall()
+        conn.close()
+
+        return result
+
+    def unvalid_enter(self, gid: int, uid: int = None):
+        conn, table_name = self._create_enter(gid)
+
+        update_sql = 'UPDATE %s SET valid = 0 WHERE valid = 1' % (table_name, )
+        if uid is not None:
+            update_sql += ' AND uid = %s' % (uid, )
+
+        conn.execute(update_sql)
+        conn.commit()
+        conn.close()
+
+    def clear_enter(self, gid: int):
+        conn, table_name = self._create_enter(gid)
+
+        drop_sql = 'DROP TABLE ' + table_name
+        conn.execute(drop_sql)
+        conn.commit()
+        conn.close()
+
+    def _create_reservation(self, gid: int) -> (sqlite3.Connection, str):
+        columns = [
+            'recid INTEGER PRIMARY KEY AUTOINCREMENT',
+            'uid INT NOT NULL',
+            'boss INT NOT NULL',
+            'time TIMESTAMP NOT NULL',
+            'remark TEXT NOT NULL',
+            'valid INT NOT NULL'
+        ]
+        return self.__create_table(gid, 'reservation', columns)
+
+    def add_reservation(self, gid: int, uid: int, boss: int):
+        conn, table_name = self._create_reservation(gid)
+
+        insert_sql = 'INSERT INTO %s VALUES (NULL, %s, %s, "%s", "", 1)' % (
+            table_name, uid, boss,  datetime.now().strftime('%Y/%m/%d %H:%M'))
+        conn.execute(insert_sql)
+        conn.commit()
+        conn.close()
+
+    def get_reservation(self, gid: int, boss: int = None) -> list:
+        conn, table_name = self._create_reservation(gid)
+        cur = conn.cursor()
+
+        select_sql = 'SELECT uid, boss FROM %s WHERE valid = 1' % (
+            table_name, )
+        if boss is not None:
+            select_sql += ' AND boss = %s' % (boss, )
+
+        cur.execute(select_sql)
+        results = cur.fetchall()
+        conn.close()
+
+        return results
+
+    def clear_reservation(self, gid: int, boss: int, uid: int = None):
+        conn, table_name = self._create_reservation(gid)
+
+        update_sql = 'UPDATE %s SET valid = 0 WHERE valid = 1 AND boss = %s' % (
+            table_name, boss)
+        if uid is not None:
+            update_sql += ' AND uid = %s' % (uid, )
+        conn.execute(update_sql)
         conn.commit()
         conn.close()
