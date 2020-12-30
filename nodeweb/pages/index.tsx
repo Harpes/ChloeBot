a5f85e1d6e21559c7f6968bd5fc4cc47c5dd7f00
Harpes <823289099@qq.com>
2020-12-30 16:10:00 +0800

    modify index page

--- nodeweb/pages/index.tsx
+++ nodeweb/pages/index.tsx
@@ -1,3 +1,8 @@
-export default function HomePage() {
-    return <div>Todo</div>;
-}
+import Image from 'next/image';
+import React from 'react';
+
+const HomePage = () => {
+    return <Image src="/chloe.gif" width={500} height={500} alt="chloe" quality={100}></Image>;
+};
+
+export default HomePage;
